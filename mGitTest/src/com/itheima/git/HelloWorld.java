18538cadaa768a56e3ea53a622634d5d2a98be48
zh <351809836@qq.com>
2024-12-26 23:05:42 +0800

    dev01 分支新增代码

--- mGitTest/src/com/itheima/git/HelloWorld.java
+++ mGitTest/src/com/itheima/git/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Git Hello World!");
         System.out.println("Git Hello World!");
+        System.out.println("我是dev01分支添加的代码");
     }
 }
