5631ac06ed6a47f2655efc212739256ac988b6d0
zh <351809836@qq.com>
2024-12-26 23:02:08 +0800

    Revert "第二次使用 idea 提交代码"
    This reverts commit d7f0f833

--- mGitTest/src/com/itheima/git/HelloWorld.java
+++ mGitTest/src/com/itheima/git/HelloWorld.java
@@ -4,6 +4,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Git Hello World!");
         System.out.println("Git Hello World!");
-        System.out.println("Git Hello World!");
     }
 }
